4dc5f73d2b7396ea5b369d6745b66a22a63d9d9c
dakoraustin <dakoraustin@gmail.com>
2022-10-13 15:22:55 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,13 @@
 #include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-
 /**
  * main - Entry point
  *
  * Return: returns 1
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar,
-			2015-10-19\n", 59);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar,
+	     2015-10-19\n";
+	write(2, str1, 59);
 	return (1);
 }
