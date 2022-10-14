08a1e0a203daa81dcf6be8af0add8240e8938ea3
dakoraustin <dakoraustin@gmail.com>
2022-10-14 10:56:20 -0700

    Alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - Prints the alphabet in lower case
+  *
+  * Return: returns 0
+  */
+
+int main(void)
+
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
