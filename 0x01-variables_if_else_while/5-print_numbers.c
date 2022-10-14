dfbe0759c0f08e9b55fc579c8d735d225ee17fc6
dakoraustin <dakoraustin@gmail.com>
2022-10-14 11:34:27 -0700

    Prints single digits in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - Prints base 10 numbers
+  *
+  * Return: returns 0
+  */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
