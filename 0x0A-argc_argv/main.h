2c6f4ea98d7d5889fbf26d4f854f519277a44d6b
dakoraustin <dakoraustin@gmail.com>
2022-11-04 14:08:33 -0700

    task 0

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int main (int argc, char *argv[]);
+
+#endif#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int main (int argc, char *argv[]);
+
+#endif#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int main (int argc, char *argv[]);
+
+#endif
